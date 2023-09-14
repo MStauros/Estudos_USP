fb4c53e553c48ec8b9face67d8e2f980c211579f
Marcos Santos <marcos.vini2727@gmail.com>
2023-09-14 19:05:25 -0300

    Atualizando arquivos e adicionando Ex03

---
+++ ICC_LICC/Ex03_LICC.py
@@ -0,0 +1,15 @@
+#Resolução´
+import random
+
+#entrada = int(input())
+entrada = random.randint(1, 10)
+
+def lista_inicial(tam):
+    lista = [0 for i in range(tam)]
+    
+    return lista
+
+
+
+print(entrada)
+print(lista_inicial(entrada))
